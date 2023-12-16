75a081a762daed6bd8c9a49337abd806301ce946
Rakib0061 <rakibulislam0061@gmail.com>
2023-12-17 03:28:01 +0600

    add the flag change featcher

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const curncy_li = document.querySelectorAll(".curncy_li");
+const curncy_flg = document.querySelectorAll(".curncy_flg");
 const curncy_ul = document.getElementById("curncy_ul");
 const curncy_btn = document.getElementById("curncy_btn");
 
@@ -9,9 +10,14 @@ curncy_btn.addEventListener("click", () => {
 
 curncy_li.forEach(vlu => {
     vlu.addEventListener("click", () => {
-        let x = vlu.getAttribute("data-title");
+        curncy_btn.children[1].innerHTML = vlu.getAttribute("data-title");
         
-        curncy_btn.children[1].innerHTML = x;
-
+        curncy_flg.forEach(flg => {
+            if (vlu.getAttribute("data-title") === flg.getAttribute("data-title")) {
+                flg.classList.add("active");
+            } else {
+                flg.classList.remove("active");
+            }
+        })
     });
 });
