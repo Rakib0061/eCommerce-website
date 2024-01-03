d4db195605c0cf3e849fc808eae0db90a13d05bd
Rakib0061 <rakibulislam0061@gmail.com>
2024-01-03 11:23:07 +0600

    add outside click handiling feather

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ const curncy_ul = document.getElementById("curncy_ul");
 const curncy_btn = document.getElementById("curncy_btn");
 
 curncy_btn.addEventListener("click", () => {
+    // for toggle click handiling 
     curncy_ul.classList.toggle("d-none");
     curncy_btn.children[2].style.transform != "rotate(180deg)" ?  curncy_btn.children[2].style.transform = "rotate(180deg)" :  curncy_btn.children[2].style.transform = "rotate(0deg)";
+});
+
+document.addEventListener("click", e => {
+    // outside click handiling for only display none
+    if (!curncy_ul.contains(e.target) && !curncy_btn.contains(e.target)) {
+        curncy_ul.classList.add("d-none");
+        curncy_btn.children[2].style.transform != "rotate(180deg)" ?  curncy_btn.children[2].style.transform = "rotate(180deg)" :  curncy_btn.children[2].style.transform = "rotate(0deg)";
+    }
 });
 
 curncy_li.forEach(vlu => {
